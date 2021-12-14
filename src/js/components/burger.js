128ec133af78d656800b9dc3e034c96d13829e62
Yurii <pidlypniyuyra@gmai.com>
2021-12-14 12:52:24 +0200

    Підключено шрифти та зроблена шапка

---
+++ src/js/components/burger.js
@@ -0,0 +1,11 @@
+//Адаптив шапки
+let burger = document.querySelector('.header__burger');
+let menu = document.querySelector('.navbar');
+let body = document.body;
+if(burger){
+  burger.addEventListener('click',()=>{
+    burger.classList.toggle('active')
+    menu.classList.toggle('active')
+    body.classList.toggle('lock')
+  })
+}
